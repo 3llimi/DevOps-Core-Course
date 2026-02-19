1f56b010d223471f774c913b26851dd3efa28dfa
Baha Alimi <a.alimi@innopolis.university>
2026-02-19 22:47:57 +0300

    terraform fix

--- terraform/github.tf
+++ terraform/github.tf
@@ -3,11 +3,11 @@ provider "github" {
 }
 
 resource "github_repository" "course_repo" {
-  name         = "DevOps-Core-Course"
-  description  = "🚀Production-grade DevOps course: 18 hands-on labs covering Docker, Kubernetes, Helm, Terraform, Ansible, CI/CD, GitOps (ArgoCD), monitoring (Prometheus/Grafana), and more. Build real-world skills with progressive delivery, secrets management, and cloud-native deployments."
-  visibility   = "public"
-  has_issues   = false
-  has_wiki     = true
+  name          = "DevOps-Core-Course"
+  description   = "🚀Production-grade DevOps course: 18 hands-on labs covering Docker, Kubernetes, Helm, Terraform, Ansible, CI/CD, GitOps (ArgoCD), monitoring (Prometheus/Grafana), and more. Build real-world skills with progressive delivery, secrets management, and cloud-native deployments."
+  visibility    = "public"
+  has_issues    = false
+  has_wiki      = true
   has_downloads = true
   has_projects  = true
 }
