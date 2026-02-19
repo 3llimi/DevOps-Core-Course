fb6f21d7c96a1bafb798bff4848a616f63327bd5
Baha Alimi <a.alimi@innopolis.university>
2026-02-19 22:50:10 +0300

    fix(terraform): add required_version and variable types for tflint

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,23 @@
 variable "vm_host" {
   description = "VM IP address"
+  type        = string
   default     = "127.0.0.1"
 }
 
 variable "vm_port" {
   description = "SSH port"
+  type        = number
   default     = 2222
 }
 
 variable "vm_user" {
   description = "SSH username"
+  type        = string
   default     = "vagrant"
 }
 
 variable "ssh_private_key_path" {
   description = "Path to SSH private key"
+  type        = string
   default     = "../vagrant/.vagrant/machines/default/virtualbox/private_key"
 }
